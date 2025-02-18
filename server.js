4aa6138a62b205623fd98c19bd8778f10f75f1d7
Ritesh Kadam <srivelrefinrry.gmail.com>
2025-02-18 15:38:12 +0530

    module export added

--- server.js
+++ server.js
@@ -85,5 +85,7 @@ app.get('/version', (req, res) => {
 });
 
 // ✅ Start Server
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+//const PORT = process.env.PORT || 3000;
+//app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+
+module.exports = app
